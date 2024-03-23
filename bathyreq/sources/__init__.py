34f3d1709fd452f7c8dfa5dc9dfc925cbc592489
William Jenkins <wfjenkins@ucsd.edu>
2024-03-22 18:12:03 -0700

    Update license

--- bathyreq/sources/__init__.py
+++ bathyreq/sources/__init__.py
@@ -1,2 +1,3 @@
+# -*- coding: utf-8 -*-
 """Subpackage for constructing data sources.
 """
--- bathyreq/sources/blue_topo.py
+++ bathyreq/sources/blue_topo.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# -*- coding: utf-8 -*-
-
 # MIT License
 
-# Copyright (c) 2023 William Jenkins
+# Copyright (c) 2023-2024 William Jenkins
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
